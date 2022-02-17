9ab99e0112d2ae6fad0cac04d7b3511a5c72709f
WoeOm <1015996366@qq.com>
2022-02-17 11:09:08 +0800

    adapt to compilation env

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {Router, Route, Switch} from "react-router-dom";
 import {asyncComponent} from "./components/AsyncComponent";
-import { createBrowserHistory} from "history";
+import { createHashHistory} from "history";
 const AsyncHome = asyncComponent(import("./page/Home"));
 const AsyncNotFound = asyncComponent(import("./page/NotFound"));
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 
 
 export default () =>
